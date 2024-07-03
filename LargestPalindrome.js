f8924f5214e6984bcd45ba204bee5bb54f589176
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-07-03 19:54:26 +0530

    first commit

---
+++ LargestPalindrome.js
@@ -0,0 +1,19 @@
+let Value = prompt("Enter The Palindrome")
+
+// console.log(Value);
+function LargestPalindrome(Value) {
+    let reversearr = []
+    let Original = Value
+
+    for (let i = Value.length - 1; i >= 0; i--) {
+        reversearr.push(Value[i])
+    }
+    let reversevalue = reversearr.join('')
+    if (Original == reversevalue) {
+        return "It is a Palindrome"
+    }else{
+        return "It is not a Palindrome"
+    }
+
+}
+console.log(LargestPalindrome(Value));
